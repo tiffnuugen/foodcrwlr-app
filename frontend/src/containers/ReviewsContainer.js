480792549011e18a098847352b0c7fecab45241c
tiffnuugen <tiffanynuugen@gmail.com>
2020-01-12 11:45:24 -0500

    build out reviews container

---
+++ frontend/src/containers/ReviewsContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ReviewsContainer extends Component {
+  render() {
+    return <div>ReviewsContainer</div>;
+  }
+}
+
+export default ReviewsContainer;
