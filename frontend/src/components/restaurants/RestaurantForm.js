3fa30c3af4b6460a32e85080aa9c3b619be4f9ce
tiffnuugen <tiffanynuugen@gmail.com>
2020-01-14 16:59:35 -0500

    build out login form

--- frontend/src/components/restaurants/RestaurantForm.js
+++ frontend/src/components/restaurants/RestaurantForm.js
@@ -3,10 +3,29 @@ import React, { Component } from 'react';
 class RestaurantForm extends Component {
   render() {
     return (
-      <form>
-        <input type='text' placeholder='name' />
-        <input type='button' />
-      </form>
+      <div className='ui center aligned container'>
+        <div className='ui centered card'>
+          <div className='content'>
+            <div className='ui form'>
+              <div className='field'>
+                <label>Username</label>
+                <div className='ui left icon input'>
+                  <input type='text' placeholder='Username' />
+                  <i className='user icon'></i>
+                </div>
+              </div>
+              <div className='field'>
+                <label>Password</label>
+                <div className='ui left icon input'>
+                  <input type='password' />
+                  <i className='lock icon'></i>
+                </div>
+              </div>
+              <div className='ui blue submit button'>Login</div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
