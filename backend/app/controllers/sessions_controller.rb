ae8ba33d72135694dfa7cd266168384ffa84d964
tiffnuugen <tiffanynuugen@gmail.com>
2020-01-28 01:58:28 -0500

    add user concern

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  include CurrentUserConcern
+
   def create
     user = User.find_by(username: params['user']['username']).try(:authenticate, params['user']['password'])
     if user
@@ -12,5 +14,23 @@ class SessionsController < ApplicationController
         render json: { status: 401 }
     end
   end
-end
- 
+
+  def logged_in
+    if @current_user
+      render json: {
+        logged_in: true,
+        user: @current_user
+      }
+    end
+  else
+    render json: {
+      logged_in: false
+    }
+  end
+
+  def logout
+    reset_session
+    render json: { status: 200, logged_out: true }
+  end
+  
+end 
