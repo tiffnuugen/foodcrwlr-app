d00c4fbe7569b9bbd8fe129fb09d67bebd9b1129
tiffnuugen <tiffanynuugen@gmail.com>
2020-01-28 02:12:38 -0500

    update session store

--- backend/config/initializers/session_store.rb
+++ backend/config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_foodcrwlr_app", domain: "http://localhost:3000"
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: "_foodcrwlr_app", domain: "https://foodcrwlr-app.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "_foodcrwlr_app"
+end
