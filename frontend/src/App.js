08658c24db089b11d2cf6cf7fce04762f5ea6450
tiffnuugen <tiffanynuugen@gmail.com>
2020-02-01 18:11:24 -0500

    add routes and connect redux to component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
-import './App.css';
-// import UsersContainer from './containers/UsersContainer';
-import RestaurantsContainer from './containers/RestaurantsContainer';
+import { connect } from 'react-redux';
+
+import './assets/stylesheets/App.css';
+import { Route, Redirect, Switch } from 'react-router-dom';
+
+import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
+import Home from './components/views/Home';
+
+import { loginUser, logoutUser } from './actions/authActions';
 
 class App extends Component {
   render() {
+    console.log('app:', this);
     return (
       <div className='app'>
-        <RestaurantsContainer />
+        <Switch>
+          <Route path='/login'>
+            <Login loginUser={this.props.loginUser} />
+          </Route>
+          <Route path='/signup'>
+            <Signup loginUser={this.props.loginUser} />
+          </Route>
+          <Route path='/home'>
+            <Home
+              username={this.props.username}
+              loginStatus={this.props.loginStatus}
+              loginUser={this.props.loginUser}
+              logoutUser={this.props.logoutUser}
+            />
+          </Route>
+          <Redirect to='/login' />
+        </Switch>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  username: state.user.username,
+  loginStatus: state.loginStatus
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  loginUser: (user) => dispatch(loginUser(user)),
+  logoutUser: () => dispatch(logoutUser())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
