ba173228bcc15dc2d20b6c10fa3fc04e700fda91
tiffnuugen <tiffanynuugen@gmail.com>
2020-02-01 18:22:10 -0500

    add browser router, provider, and store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import './assets/stylesheets/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter } from 'react-router-dom';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import authReducer from './reducers/authReducer';
+
+const store = createStore(
+  authReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
