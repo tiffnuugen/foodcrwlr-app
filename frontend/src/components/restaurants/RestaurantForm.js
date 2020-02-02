c97ebc04b78702752f5e64c18576c96918a8f8b4
tiffnuugen <tiffanynuugen@gmail.com>
2020-02-02 15:58:22 -0500

    add text and form for restaurant components as placeholders for now

--- frontend/src/components/restaurants/RestaurantForm.js
+++ frontend/src/components/restaurants/RestaurantForm.js
@@ -3,29 +3,10 @@ import React, { Component } from 'react';
 class RestaurantForm extends Component {
   render() {
     return (
-      <div className='ui center aligned container'>
-        <div className='ui centered card'>
-          <div className='content'>
-            <div className='ui form'>
-              <div className='field'>
-                <label>Username</label>
-                <div className='ui left icon input'>
-                  <input type='text' placeholder='Username' />
-                  <i className='user icon'></i>
-                </div>
-              </div>
-              <div className='field'>
-                <label>Password</label>
-                <div className='ui left icon input'>
-                  <input type='password' />
-                  <i className='lock icon'></i>
-                </div>
-              </div>
-              <div className='ui blue submit button'>Login</div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <form>
+        <input type='text' />
+        <input type='button' />
+      </form>
     );
   }
 }
