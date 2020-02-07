b32d600d8c7314567fc519aa6d71b8e5cdd92d4d
tiffnuugen <tiffanynuugen@gmail.com>
2020-02-06 23:01:05 -0500

    create restaurant model

---
+++ backend/db/migrate/20200205050135_create_restaurants.rb
@@ -0,0 +1,12 @@
+class CreateRestaurants < ActiveRecord::Migration[6.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :category
+      t.string :location
+      t.belongs_to :user
+
+      t.timestamps
+    end
+  end
+end
