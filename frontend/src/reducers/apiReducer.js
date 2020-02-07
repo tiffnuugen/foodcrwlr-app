5573583ce9135e69a8fbcb62524b7fe306af257f
tiffnuugen <tiffanynuugen@gmail.com>
2020-02-06 23:25:13 -0500

    create api reducer

---
+++ frontend/src/reducers/apiReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  restaurants: [],
+  loading: false
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case 'LOADING_RESTAURANTS':
+
+    default:
+      return state;
+  }
+};
