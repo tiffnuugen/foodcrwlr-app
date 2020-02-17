be77313d54b54a62213bd856381cabc9b1f50a98
tiffnuugen <tiffanynuugen@gmail.com>
2020-02-17 16:24:15 -0500

    import and use semantic ui react and display all restaurants

--- frontend/src/components/restaurants/Restaurants.js
+++ frontend/src/components/restaurants/Restaurants.js
@@ -1,9 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Card, Image, Icon } from 'semantic-ui-react';
 
-class Restaurants extends Component {
-  render() {
-    return <ul>restaurants</ul>;
-  }
-}
+const Restaurants = ({ restaurants }) => {
+  return (
+    <Card.Group itemsPerRow={5}>
+      {restaurants.map((restaurant) => (
+        <Card
+          key={restaurant.id}
+          as={Link}
+          to={`/restaurants/${restaurant.id}`}
+        >
+          <Image src={restaurant.image_url} />
+          <Card.Content>
+            <Card.Header>{restaurant.name}</Card.Header>
+            <Card.Meta>
+              Price: {restaurant.price === undefined ? 'N/A' : restaurant.price}{' '}
+              | Rating: {restaurant.rating}
+            </Card.Meta>
+          </Card.Content>
+          <Card.Content extra>
+            <Icon name='phone' />
+            {restaurant.display_phone === ''
+              ? 'Not Available'
+              : restaurant.display_phone}
+          </Card.Content>
+        </Card>
+      ))}
+    </Card.Group>
+  );
+};
 
 export default Restaurants;
