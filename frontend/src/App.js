00a3eae7f2898b427d0f6dc11e6a2b9c6b559385
tiffnuugen <tiffanynuugen@gmail.com>
2020-02-17 15:17:20 -0500

    add restaurants and single restaurant routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-
 import './assets/stylesheets/App.css';
+import { connect } from 'react-redux';
 import { Route, Redirect, Switch } from 'react-router-dom';
 
 import Login from './components/auth/Login';
 import Signup from './components/auth/Signup';
 import Home from './components/views/Home';
+import Restaurants from './components/restaurants/Restaurants';
+import Restaurant from './components/restaurants/Restaurant';
 
 import { loginUser, logoutUser } from './actions/authActions';
 
 class App extends Component {
   render() {
-    console.log('app:', this);
     return (
       <div className='app'>
         <Switch>
@@ -22,13 +22,18 @@ class App extends Component {
           <Route path='/signup'>
             <Signup loginUser={this.props.loginUser} />
           </Route>
-          <Route path='/home'>
+          <Route path='/'>
             <Home
-              username={this.props.username}
               loginStatus={this.props.loginStatus}
               loginUser={this.props.loginUser}
               logoutUser={this.props.logoutUser}
             />
+          </Route>
+          <Route exact path='/restaurants'>
+            <Restaurants />
+          </Route>
+          <Route>
+            <Restaurant path='/restaurants/:id' />
           </Route>
           <Redirect to='/login' />
         </Switch>
@@ -38,7 +43,6 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => ({
-  username: state.authReducer.user.username,
   loginStatus: state.authReducer.loginStatus
 });
 
