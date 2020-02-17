28426c99c6a705b05845c0e39cceda03b81c8d9f
tiffnuugen <tiffanynuugen@gmail.com>
2020-02-17 16:48:02 -0500

    build api reducer

--- frontend/src/reducers/apiReducer.js
+++ frontend/src/reducers/apiReducer.js
@@ -6,7 +6,19 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case 'LOADING_RESTAURANTS':
-
+      console.log('restaurants loaded!', action);
+      return {
+        ...state,
+        restaurants: [...state.restaurants],
+        loading: true
+      };
+    case 'ADD_RESTAURANTS':
+      console.log('restaurants added!', action);
+      return {
+        ...state.restaurants,
+        restaurants: action.restaurants,
+        loading: false
+      };
     default:
       return state;
   }
