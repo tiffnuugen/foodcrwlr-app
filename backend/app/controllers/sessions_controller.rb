78f5bef9e186b1ef2167ea77255b115d3f32a66c
tiffnuugen <tiffanynuugen@gmail.com>
2020-02-22 18:33:11 -0500

    format json object from single-line to multi-line

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -12,16 +12,16 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params['user']['username']).try(:authenticate, params['user']['password'])
     if user
       session[:user_id] = user.id
-        render json: {
-          status: :created,
-          logged_in: true,
-          user: user
-        }
-      else
-        render json: { status: 401 }
+      render json: {
+        status: :created,
+        logged_in: true,
+        user: user
+      }
+    else
+      render json: { status: 401 }
     end
   end
-
+  
   def logged_in
     if @current_user
       render json: {
@@ -34,10 +34,13 @@ class SessionsController < ApplicationController
       }
     end
   end
-
+  
   def logout
     reset_session
-    render json: { status: 200, logged_out: true }
+    render json: { 
+      status: 200, 
+      logged_out: true
+    }
   end
   
 end 
