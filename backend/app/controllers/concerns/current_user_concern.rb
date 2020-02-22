90472c7483120dd1190e7af3c9d36d9410678649
tiffnuugen <tiffanynuugen@gmail.com>
2020-02-22 18:54:43 -0500

    change method from find to find_by_id

--- backend/app/controllers/concerns/current_user_concern.rb
+++ backend/app/controllers/concerns/current_user_concern.rb
@@ -7,7 +7,7 @@ module CurrentUserConcern
 
   def set_current_user
     if session[:user_id]
-      @current_user = User.find(session[:user_id])
+      @current_user = User.find_by_id(session[:user_id])
     end
   end
 end
