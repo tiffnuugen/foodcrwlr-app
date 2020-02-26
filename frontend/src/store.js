8470e649c69fbb36fcb2af1601ee9a0eebf76e94
tiffnuugen <tiffanynuugen@gmail.com>
2020-02-25 19:26:05 -0500

    change storage engine of redux store from session storage to local storage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { persistStore, persistReducer } from 'redux-persist';
-import storageSession from 'redux-persist/lib/storage/session';
+import storage from 'redux-persist/lib/storage';
 
 import rootReducer from './reducers/rootReducer';
 
@@ -9,7 +9,7 @@ const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const persistConfig = {
   key: 'root',
-  storage: storageSession,
+  storage: storage,
   whitelist: ['api']
 };
 
