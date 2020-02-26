84b63d55d342c30a7ce8e19a3ac6ee2388b7827c
tiffnuugen <tiffanynuugen@gmail.com>
2020-02-26 16:17:31 -0500

    create clear restaurants action creator

--- frontend/src/actions/apiActions.js
+++ frontend/src/actions/apiActions.js
@@ -19,3 +19,7 @@ export const fetchRestaurants = (searchValues) => {
       .catch((error) => console.log(error.response));
   };
 };
+
+export const clearRestaurants = () => ({
+  type: 'CLEAR_RESTAURANTS'
+});
