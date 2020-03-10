f654c6b8cb36e135bd0e052e2f9728f948fbeb20
tiffnuugen <tiffanynuugen@gmail.com>
2020-03-10 06:29:52 -0400

    import and reuse reviews container in restaurant details container

--- frontend/src/containers/RestaurantDetailsContainer.js
+++ frontend/src/containers/RestaurantDetailsContainer.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import RestaurantDetails from '../components/restaurants/RestaurantDetails';
+import ReviewsContainer from './ReviewsContainer';
 
 class RestaurantDetailsContainer extends Component {
   render() {
     return (
-      <div className='ui text restaurant details container'>
-        <RestaurantDetails
-          restaurantDetails={this.props.restaurantDetails}
-          loading={this.props.loading}
-        />
-      </div>
+      <>
+        <div className='ui text restaurant details container'>
+          <RestaurantDetails
+            restaurantDetails={this.props.restaurantDetails}
+            loading={this.props.loading}
+          />
+        </div>
+        <div className='ui text reviews container'>
+          <ReviewsContainer />
+        </div>
+      </>
     );
   }
 }
