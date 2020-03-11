4a7fb1875cb280b52eb587847e5bd4de0027683b
tiffnuugen <tiffanynuugen@gmail.com>
2020-03-11 14:49:53 -0400

    move reviews container inside div tag

--- frontend/src/containers/RestaurantDetailsContainer.js
+++ frontend/src/containers/RestaurantDetailsContainer.js
@@ -8,18 +8,16 @@ import ReviewsContainer from './ReviewsContainer';
 class RestaurantDetailsContainer extends Component {
   render() {
     return (
-      <>
-        <div className='ui text restaurant details container'>
-          <RestaurantDetails
-            restaurantDetails={this.props.restaurantDetails}
-            loading={this.props.loading}
-          />
-          <Divider section horizontal>
-            <Header as='h2'>REVIEWS</Header>
-          </Divider>
-        </div>
+      <div className='ui text restaurant details container'>
+        <RestaurantDetails
+          restaurantDetails={this.props.restaurantDetails}
+          loading={this.props.loading}
+        />
+        <Divider section horizontal>
+          <Header as='h2'>REVIEWS</Header>
+        </Divider>
         <ReviewsContainer />
-      </>
+      </div>
     );
   }
 }
