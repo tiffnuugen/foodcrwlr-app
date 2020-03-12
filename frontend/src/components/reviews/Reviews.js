fe7a8b84ad7aeec438eb7c5220809a8497abec02
tiffnuugen <tiffanynuugen@gmail.com>
2020-03-12 00:36:09 -0400

    create reviews component

--- frontend/src/components/reviews/Reviews.js
+++ frontend/src/components/reviews/Reviews.js
@@ -1,1 +1,47 @@
+import React, { Component } from 'react';
+import {
+  Icon,
+  Comment,
+  Form,
+  Button,
+  Message,
+  Divider,
+  Header
+} from 'semantic-ui-react';
 
+import ReviewForm from './ReviewForm';
+
+class Reviews extends Component {
+  render() {
+    return (
+      <Comment.Group>
+        <ReviewForm />
+        <Divider section horizontal>
+          <Header as='h2'>REVIEWS</Header>
+        </Divider>
+        {/* <Message>
+          <Message.Header>No Reviews</Message.Header>
+          <p>
+            It looks like there are no reviews here yet. Be the first to leave
+            one here.
+          </p>
+        </Message> */}
+        <Comment>
+          {/* <Comment.Avatar src='' /> */}
+          <Comment.Content>
+            <Comment.Author as='a'>Matt</Comment.Author>
+            <Comment.Metadata>
+              <div>Today at 5:42PM</div>
+            </Comment.Metadata>
+            <Comment.Text>How artistic!</Comment.Text>
+            <Comment.Actions>
+              <Comment.Action>Edit</Comment.Action>
+            </Comment.Actions>
+          </Comment.Content>
+        </Comment>
+      </Comment.Group>
+    );
+  }
+}
+
+export default Reviews;
