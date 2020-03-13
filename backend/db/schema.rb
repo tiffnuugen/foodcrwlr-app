b5343b44ddc62da85aad375e12b0f97fbe019302
tiffnuugen <tiffanynuugen@gmail.com>
2020-03-13 16:09:18 -0400

    add user id to reviews and restaurants table and run their migrations

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,19 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_05_050135) do
+ActiveRecord::Schema.define(version: 2020_03_13_195204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
-    t.string "category"
-    t.string "location"
-    t.bigint "user_id"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_restaurants_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "text"
+    t.integer "rating"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
