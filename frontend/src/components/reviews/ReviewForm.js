3b5c2ae3e2ea2597cda6a006afbc51ee414a03c3
tiffnuugen <tiffanynuugen@gmail.com>
2020-03-13 15:30:19 -0400

    connect review form to redux store, take user input from review form, and send to redux store

--- frontend/src/components/reviews/ReviewForm.js
+++ frontend/src/components/reviews/ReviewForm.js
@@ -1,12 +1,59 @@
 import React, { Component } from 'react';
-import { Form, Button, Icon } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { Form, TextArea, Button, Icon, Rating } from 'semantic-ui-react';
+
+import { addReview } from '../../actions/reviewActions';
 
 class ReviewForm extends Component {
+  state = {
+    text: '',
+    rating: 0
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      text: e.target.value
+    });
+  };
+
+  handleRate = (e, { rating }) => {
+    this.setState({
+      rating
+    });
+  };
+
+  handleSubmit = () => {
+    const review = {
+      // userId: this.props.userId,
+      // restaurantId: this.props.restaurantId,
+      text: this.state.text,
+      rating: this.state.rating
+    };
+    this.props.addReview(review);
+    this.setState({
+      text: '',
+      rating: 0
+    });
+  };
+
   render() {
     return (
-      <Form reply>
-        <Form.TextArea placeholder='Write a review...' />
-        <Button size='large' floated='right' basic color='teal'>
+      <Form className='review' onSubmit={this.handleSubmit}>
+        <strong>Select a rating:</strong>{' '}
+        <Rating
+          clearable
+          size='huge'
+          rating={this.state.rating}
+          maxRating={5}
+          onRate={this.handleRate}
+        />
+        <TextArea
+          name='text'
+          placeholder='Write a review...'
+          value={this.state.text}
+          onChange={this.handleChange}
+        />
+        <Button type='submit' floated='right' basic color='teal'>
           <Icon name='add' />
           Add Review
         </Button>
@@ -15,4 +62,13 @@ class ReviewForm extends Component {
   }
 }
 
-export default ReviewForm;
+// const mapStateToProps = (state) => ({
+//   restaurantId: state.api.restaurantDetails.id,
+//   userId: state.auth.user.id
+// });
+
+const mapDispatchToProps = (dispatch) => ({
+  addReview: (review) => dispatch(addReview(review))
+});
+
+export default connect(null, mapDispatchToProps)(ReviewForm);
