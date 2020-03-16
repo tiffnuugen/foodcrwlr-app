df5fe915432428986718f9b7bcf254e6d25c4b70
tiffnuugen <tiffanynuugen@gmail.com>
2020-03-16 00:56:55 -0400

    build index, show, and create actions

--- backend/app/controllers/reviews_controller.rb
+++ backend/app/controllers/reviews_controller.rb
@@ -1,3 +1,27 @@
 class ReviewsController < ApplicationController
-  
+  before_action :set_review, only: [:show, :destroy, :update]
+
+  def index
+    reviews = Review.all
+    render json: reviews, status: 200
+  end  
+
+  def show
+    render json: @review, status: 200
+  end
+
+  def create
+    review = Review.create(review_params)
+    render json: review, status: 201
+  end
+
+
+  private
+  def review_params
+    params.require(:review).permit(:text, :rating, :user_id)
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
 end
