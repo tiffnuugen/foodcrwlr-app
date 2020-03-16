ed8e178925b18c82e2c6da70eceff99e915a9d6d
tiffnuugen <tiffanynuugen@gmail.com>
2020-03-16 02:09:18 -0400

    add reviews and users routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root to: "static#home"
   get :logged_in, to: 'sessions#logged_in'
-  get :restaurants, to: 'restaurants#search'
+  # get :restaurants, to: 'restaurants#search'
   resources :sessions, only: [:index, :create]
+  resources :reviews
+  resources :users
+  # resources :restaurants
   resources :registrations, only: [:index, :create]
   delete :logout, to: 'sessions#logout'
 end
