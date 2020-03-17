9571b7bcc029eebcaecbec60b31f047ed955cc7e
tiffnuugen <tiffanynuugen@gmail.com>
2020-03-17 02:09:09 -0400

    add restaurant id attribute and belongs_to restaurant association to review serializer

--- backend/app/serializers/restaurant_serializer.rb
+++ backend/app/serializers/restaurant_serializer.rb
@@ -1,3 +1,5 @@
 class RestaurantSerializer < ActiveModel::Serializer
   attributes :id, :name
+  has_many :reviews
+  has_many :users
 end
--- backend/app/serializers/review_serializer.rb
+++ backend/app/serializers/review_serializer.rb
@@ -1,5 +1,5 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :text, :rating, :user_id
+  attributes :id, :text, :rating, :user_id, :restaurant_id
   belongs_to :user
-  # belongs_to :restaurant
+  belongs_to :restaurant
 end
