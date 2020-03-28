024fca93e4e192815169d014ed52e907520148af
tiffnuugen <tiffanynuugen@gmail.com>
2020-03-28 11:59:48 -0400

    build index, show, and create actions as well as private methods

--- backend/app/controllers/restaurants_controller.rb
+++ backend/app/controllers/restaurants_controller.rb
@@ -1,3 +1,27 @@
 class RestaurantsController < ApplicationController
+  before_action :set_review, only: [:show, :destroy, :update]
+
+  def index
+    restaurants = Restaurant.all
+    render json: restaurants, status: 200
+  end
+
+  def show
+    render json: @restaurant, status: 200
+  end
+  
+  def create
+    restaurant = Restaurant.create(restaurant_params)
+    render json: restaurant, status: 201
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :yelp_id)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
   
 end
