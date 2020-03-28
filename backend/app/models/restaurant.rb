678baade35226003f746d81c16d402e826235656
tiffnuugen <tiffanynuugen@gmail.com>
2020-03-28 12:27:33 -0400

    set yelp id as primary key on reviews table

--- backend/app/models/restaurant.rb
+++ backend/app/models/restaurant.rb
@@ -3,5 +3,7 @@ class Restaurant < ApplicationRecord
   validates :name, uniqueness: true
   validates :yelp_id, presence: true
   validates :yelp_id, uniqueness: true
+
+  has_many :reviews, primary_key: 'yelp_id'
   has_many :users, through: :reviews
 end
