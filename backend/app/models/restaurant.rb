ce40250d541e58f873498141fe665f758fe3ad2f
tiffnuugen <tiffanynuugen@gmail.com>
2020-03-28 12:24:25 -0400

    validate presence and uniqueness of both name and yelp id for restaurant model

--- backend/app/models/restaurant.rb
+++ backend/app/models/restaurant.rb
@@ -1,4 +1,7 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :yelp_id, presence: true
+  validates :yelp_id, uniqueness: true
   has_many :users, through: :reviews
 end
