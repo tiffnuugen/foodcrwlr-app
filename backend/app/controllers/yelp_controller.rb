d370f142a23d809d00e357adbd274c5aa30daf29
tiffnuugen <tiffanynuugen@gmail.com>
2020-03-30 14:50:20 -0400

    build search action of yelp controller

--- backend/app/controllers/yelp_controller.rb
+++ backend/app/controllers/yelp_controller.rb
@@ -1,13 +1,13 @@
 class YelpController < ApplicationController
   def search
-    res = Faraday.get('https://api.yelp.com/v3/businesses/search') do |req|
+    term = params[:term]
+    location = params[:location]
+    res = Faraday.get("https://api.yelp.com/v3/businesses/search?term=#{term}&location=#{location}") do |req|
       req.headers['Authorization'] = "Bearer #{ENV['API_KEY']}"
       req.headers['Content-Type'] = 'application/json'
-      # req.params['location'] = 'nyc'
     end
     search_results = JSON.parse(res.body)
     render json: search_results
-    # byebug
   end
 end
 
