4e3896f7967199d04b65ea93b74fe7aedf414f15
tiffnuugen <tiffanynuugen@gmail.com>
2020-03-31 14:43:29 -0400

    use only option to add only necessary routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   root to: "static#home"
   get :logged_in, to: 'sessions#logged_in'
-  get '/search', to: 'yelp#search'
   resources :sessions, only: [:index, :create]
-  resources :reviews
-  resources :users
-  resources :restaurants
+  resources :reviews, only: [:index, :show, :create, :update, :destroy]
+  resources :users, only: [:index, :show]
+  resources :restaurants, only: [:index, :show, :create, :update, :destroy]
   resources :registrations, only: [:index, :create]
   delete :logout, to: 'sessions#logout'
 end
