f92c21b8a87045d88ade1316efc7b500254e815e
tiffnuugen <tiffanynuugen@gmail.com>
2020-03-31 15:49:27 -0400

    create search route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "static#home"
   get :logged_in, to: 'sessions#logged_in'
+  get '/search', to: 'yelp_search_queries#search'
   resources :sessions, only: [:index, :create]
   resources :reviews, only: [:index, :show, :create, :update, :destroy]
   resources :users, only: [:index, :show]
