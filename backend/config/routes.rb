f3e03524d8dbd066ea7c50dcc8e1e9c5a319da38
tiffnuugen <tiffanynuugen@gmail.com>
2020-04-02 19:06:38 -0400

    create search and details routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: "static#home"
   get :logged_in, to: 'sessions#logged_in'
-  post '/search', to: 'yelp_search_queries#search'
+  post '/search', to: 'yelp#search'
+  post '/details', to: 'yelp#display'
   resources :sessions, only: [:index, :create]
   resources :reviews, only: [:index, :show, :create, :update, :destroy]
   resources :users, only: [:index, :show]
