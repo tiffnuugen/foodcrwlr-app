e4cf058952f85224de59c25b1d72bb95b2e280a2
tiffnuugen <tiffanynuugen@gmail.com>
2020-04-04 17:41:58 -0400

    remove name attribute from restaurant validation

--- backend/app/models/restaurant.rb
+++ backend/app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  validates :name, :yelp_id, presence: true, uniqueness: true
+  validates :yelp_id, presence: true, uniqueness: true
   has_many :reviews, primary_key: 'yelp_id'
   has_many :users, through: :reviews
 end
