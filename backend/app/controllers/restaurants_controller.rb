b40b7f13a6e7e03716db95b9bd5d56611854e5de
tiffnuugen <tiffanynuugen@gmail.com>
2020-04-04 17:34:11 -0400

    change method name from set review to set restaurant for before action filter

--- backend/app/controllers/restaurants_controller.rb
+++ backend/app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  before_action :set_review, only: [:show, :destroy, :update]
+  before_action :set_restaurant, only: [:show, :destroy, :update]
 
   def index
     restaurants = Restaurant.all
