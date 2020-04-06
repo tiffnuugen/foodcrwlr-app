1f035a78c3fc6866c0655293a7db5e9ab3387a32
tiffnuugen <tiffanynuugen@gmail.com>
2020-04-06 13:26:37 -0400

    add created_at attribute to review serializer

--- backend/app/serializers/review_serializer.rb
+++ backend/app/serializers/review_serializer.rb
@@ -1,5 +1,5 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :text, :rating, :user_id, :restaurant_id
+  attributes :id, :text, :rating, :created_at, :user_id, :restaurant_id
   belongs_to :user
   belongs_to :restaurant
 end
