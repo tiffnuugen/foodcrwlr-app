312b60f4d5477eb2d79121936e7940d2cfdf9086
tiffnuugen <tiffanynuugen@gmail.com>
2020-04-07 14:36:08 -0400

    make post request to add review and associate with restaurant

--- frontend/src/components/reviews/ReviewForm.js
+++ frontend/src/components/reviews/ReviewForm.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { connect } from 'react-redux';
 import { Form, TextArea, Button, Icon, Rating } from 'semantic-ui-react';
 
-import { addReview } from '../../actions/reviewActions';
+import { addReview } from '../../actions/apiActions';
 
 class ReviewForm extends Component {
   state = {
@@ -22,14 +23,25 @@ class ReviewForm extends Component {
     });
   };
 
-  handleSubmit = () => {
-    const review = {
-      // userId: this.props.userId,
-      // restaurantId: this.props.restaurantId,
-      text: this.state.text,
-      rating: this.state.rating
-    };
-    this.props.addReview(review);
+  handleSubmit = async () => {
+    const { text, rating } = this.state;
+    const { userId, restaurantId, restaurantName, yelpId } = this.props;
+
+    await axios.post('http://localhost:3001/restaurants', {
+      restaurant: {
+        name: restaurantName,
+        yelp_id: yelpId
+      }
+    });
+    const review = await axios.post('http://localhost:3001/reviews', {
+      review: {
+        text: text,
+        rating: rating,
+        user_id: userId,
+        restaurant_id: restaurantId
+      }
+    });
+    this.props.addReview(review.data);
     this.setState({
       text: '',
       rating: 0
@@ -62,13 +74,15 @@ class ReviewForm extends Component {
   }
 }
 
-// const mapStateToProps = (state) => ({
-//   restaurantId: state.api.restaurantDetails.id,
-//   userId: state.auth.user.id
-// });
+const mapStateToProps = (state) => ({
+  restaurantId: state.yelp.restaurantDetails.id,
+  userId: state.auth.user.id,
+  restaurantName: state.yelp.restaurantDetails.name,
+  yelpId: state.yelp.restaurantDetails.id
+});
 
 const mapDispatchToProps = (dispatch) => ({
   addReview: (review) => dispatch(addReview(review))
 });
 
-export default connect(null, mapDispatchToProps)(ReviewForm);
+export default connect(mapStateToProps, mapDispatchToProps)(ReviewForm);
