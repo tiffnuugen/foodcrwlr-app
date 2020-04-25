b0ab282440a57cf954986fb0b846309df51dada1
tiffnuugen <tiffanynuugen@gmail.com>
2020-04-25 16:24:39 -0400

    handle fetch reviews action in api reducer

--- frontend/src/reducers/apiReducer.js
+++ frontend/src/reducers/apiReducer.js
@@ -7,6 +7,9 @@ export default (state = initialState, action) => {
     case 'ADD_REVIEW':
       console.log('review added:', action);
       return { ...state.reviews, reviews: [...state.reviews, action.review] };
+    case 'FETCH_REVIEWS':
+      console.log('reviews fetched!', action);
+      return { ...state, reviews: action.reviews };
     default:
       return state;
   }
