32990259d909fe2b3bf10b78d8a20f88907dbc09
tiffnuugen <tiffanynuugen@gmail.com>
2020-04-25 16:50:02 -0400

    build and mount fetch reviews function

--- frontend/src/containers/RestaurantDetailsContainer.js
+++ frontend/src/containers/RestaurantDetailsContainer.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Divider } from 'semantic-ui-react';
+import axios from 'axios';
 
 import RestaurantDetails from '../components/restaurants/RestaurantDetails';
 import ReviewsContainer from './ReviewsContainer';
 
+import { fetchReviews } from '../actions/apiActions';
+
 class RestaurantDetailsContainer extends Component {
+  fetchReviews = () => {
+    axios
+      .get('http://localhost:3001/reviews')
+      .then((res) => this.props.fetchReviews(res.data));
+  };
+
+  componentDidMount() {
+    this.fetchReviews();
+  }
+
   render() {
     return (
       <div className='ui text restaurant details container'>
@@ -25,4 +38,11 @@ const mapStateToProps = (state) => ({
   loading: state.yelp.loading
 });
 
-export default connect(mapStateToProps)(RestaurantDetailsContainer);
+const mapDispatchToProps = (dispatch) => ({
+  fetchReviews: (reviews) => dispatch(fetchReviews(reviews))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(RestaurantDetailsContainer);
