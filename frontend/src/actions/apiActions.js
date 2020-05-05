ca873045cfa934dc1875dda488c18c9d682b337c
tiffnuugen <tiffanynuugen@gmail.com>
2020-05-05 18:02:07 -0400

    create edit review action creator

--- frontend/src/actions/apiActions.js
+++ frontend/src/actions/apiActions.js
@@ -1,14 +1,19 @@
 export const addReview = (review) => ({
   type: 'ADD_REVIEW',
-  review
+  review,
 });
 
 export const fetchReviews = (reviews) => ({
   type: 'FETCH_REVIEWS',
-  reviews
+  reviews,
 });
 
 export const deleteReview = (id) => ({
   type: 'DELETE_REVIEW',
-  id
+  id,
+});
+
+export const editReview = (review) => ({
+  type: 'EDIT_REVIEW',
+  review,
 });
