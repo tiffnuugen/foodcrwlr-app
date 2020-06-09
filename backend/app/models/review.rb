4bbed7c10a5db8b0faba96558693219f3bebde89
tiffnuugen <tiffanynuugen@gmail.com>
2020-06-09 17:54:33 -0400

    delete restaurant if there are no more reviews

--- backend/app/models/review.rb
+++ backend/app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :restaurant, optional: true, primary_key: 'yelp_id'
+  after_destroy :destroy_orphaned_restaurant
+
+  def destroy_orphaned_restaurant
+    restaurant.destroy if restaurant.reviews.empty?
+  end
 end
