d62d1498d81259fa3d1eb0c6642c0ddaf7f77e2a
tiffnuugen <tiffanynuugen@gmail.com>
2020-06-09 19:17:23 -0400

    build edit review functionality

--- frontend/src/components/reviews/Review.js
+++ frontend/src/components/reviews/Review.js
@@ -1,61 +1,190 @@
-import React from 'react';
-import { Comment, Rating } from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { Comment, Rating, Form, TextArea } from 'semantic-ui-react';
+import TextareaAutosize from 'react-textarea-autosize';
 import axios from 'axios';
 
-const renderDate = (createdAt) => {
-  const date = new Date(createdAt).toLocaleDateString();
-  return date;
-};
+class Review extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditing: false,
+      text: props.originalText,
+      rating: props.originalRating
+    };
+  }
 
-const renderTime = (createdAt) => {
-  const time = new Date(createdAt).toLocaleTimeString('en-US', {
-    hour: 'numeric',
-    minute: 'numeric',
-    hour12: true
-  });
-  return time;
-};
-
-const Review = ({
-  id,
-  text,
-  rating,
-  username,
-  createdAt,
-  currentUser,
-  deleteReview
-}) => {
-  const handleDelete = (id) => {
-    axios
-      .delete(`http://localhost:3001/reviews/${id}`)
-      .then((res) => {
-        deleteReview(res.data.review_id);
-      })
-      .catch((error) => console.log(error));
+  toggleEdit = () => {
+    this.setState({
+      isEditing: true
+    });
   };
-  return (
-    <Comment>
-      {/* <Comment.Avatar src='' /> */}
-      <Rating disabled size='large' defaultRating={rating} maxRating={5} />
-      <Comment.Content>
-        <Comment.Author as='a'>{username}</Comment.Author>
-        <Comment.Metadata>
-          {renderDate(createdAt)} at {renderTime(createdAt)}
-        </Comment.Metadata>
-        <Comment.Text>{text}</Comment.Text>
-        <Comment.Actions>
-          {currentUser === username ? (
-            <>
-              <Comment.Action>Edit</Comment.Action>
-              <Comment.Action onClick={() => handleDelete(id)}>
-                Delete
-              </Comment.Action>
-            </>
-          ) : null}
-        </Comment.Actions>
-      </Comment.Content>
-    </Comment>
-  );
-};
+
+  handleChange = (e) => {
+    this.setState({
+      text: e.target.value
+    });
+  };
+
+  handleRate = (e, { rating }) => {
+    this.setState({
+      rating
+    });
+  };
+
+  handleEdit = () => {
+    const { id, originalText, originalRating, editReview } = this.props;
+    const { text, rating } = this.state;
+    if (
+      originalText === text ||
+      originalRating === rating ||
+      originalText !== text ||
+      originalRating !== rating
+    ) {
+      axios
+        .patch(`http://localhost:3001/reviews/${id}`, {
+          review: {
+            text: text,
+            rating: rating,
+            edited: true
+          }
+        })
+        .then((res) => {
+          editReview(res.data);
+          this.setState({
+            isEditing: false
+          });
+        });
+    } else {
+      axios
+        .patch(`http://localhost:3001/reviews/${id}`, {
+          review: {
+            text: text,
+            rating: rating,
+            edited: false
+          }
+        })
+        .then((res) => {
+          editReview(res.data);
+          this.setState({
+            isEditing: false
+          });
+        });
+    }
+  };
+
+  renderDate = (createdAt) => {
+    const date = new Date(createdAt).toLocaleDateString();
+    return date;
+  };
+
+  renderTime = (createdAt) => {
+    const time = new Date(createdAt).toLocaleTimeString('en-US', {
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true
+    });
+    return time;
+  };
+
+  render() {
+    const {
+      id,
+      originalText,
+      originalRating,
+      edited,
+      username,
+      createdAt,
+      currentUser,
+      deleteReview
+    } = this.props;
+    const { text, rating, isEditing } = this.state;
+    const handleDelete = (id) => {
+      axios
+        .delete(`http://localhost:3001/reviews/${id}`)
+        .then((res) => {
+          deleteReview(res.data.review_id);
+        })
+        .catch((error) => console.log(error));
+    };
+    return (
+      <>
+        {isEditing ? (
+          <Form>
+            <Comment>
+              {/* <Comment.Avatar src='' /> */}
+              <Rating
+                clearable
+                size='huge'
+                rating={rating}
+                maxRating={5}
+                onRate={this.handleRate}
+              />
+              <Comment.Content>
+                <Comment.Author as='a'>{username}</Comment.Author>
+                <Comment.Metadata>
+                  {this.renderDate(createdAt)} at {this.renderTime(createdAt)}
+                </Comment.Metadata>
+                <Comment.Text>
+                  <TextareaAutosize
+                    name='text'
+                    value={text}
+                    rows={1}
+                    onChange={this.handleChange}
+                  />
+                </Comment.Text>
+                <Comment.Actions>
+                  {currentUser === username ? (
+                    <>
+                      <Comment.Action onClick={() => this.handleEdit()}>
+                        Done
+                      </Comment.Action>
+                      <Comment.Action onClick={() => handleDelete(id)}>
+                        Delete
+                      </Comment.Action>
+                    </>
+                  ) : null}
+                </Comment.Actions>
+              </Comment.Content>
+            </Comment>
+          </Form>
+        ) : (
+          <Comment>
+            {/* <Comment.Avatar src='' /> */}
+            <Rating
+              disabled
+              size='large'
+              defaultRating={originalRating}
+              maxRating={5}
+            />
+            <Comment.Content>
+              <Comment.Author as='a'>{username}</Comment.Author>
+              <Comment.Metadata>
+                {this.renderDate(createdAt)} at {this.renderTime(createdAt)}
+              </Comment.Metadata>
+              <Comment.Text>
+                {originalText}
+                <Comment.Metadata>
+                  {currentUser === username && edited ? '(edited)' : null}
+                </Comment.Metadata>
+              </Comment.Text>
+              <Comment.Actions>
+                {currentUser === username ? (
+                  <>
+                    <Comment.Action onClick={() => this.toggleEdit()}>
+                      Edit
+                    </Comment.Action>
+                    <Comment.Action onClick={() => handleDelete(id)}>
+                      Delete
+                    </Comment.Action>
+                  </>
+                ) : null}
+              </Comment.Actions>
+            </Comment.Content>
+          </Comment>
+        )}
+      </>
+    );
+  }
+}
 
 export default Review;
