28eee91f2761ab914eb63bf168123bf10423f173
tiffnuugen <tiffanynuugen@gmail.com>
2020-07-10 13:36:18 -0400

    return review id in custom json data

--- backend/app/controllers/reviews_controller.rb
+++ backend/app/controllers/reviews_controller.rb
@@ -14,23 +14,22 @@ class ReviewsController < ApplicationController
     review = Review.create(review_params)
     render json: review, status: 201
   end
-
+  
   def update
     @review.update(review_params)
     render json: @review, status: 200
   end
-
+  
   def destroy
-    review_id = @review.id
     @review.destroy
-    render json: {message: "review deleted", review_id: review_id}
+    render json: {message: "review deleted", id: @review.id}
   end
-
+  
   private
   def review_params
     params.require(:review).permit(:text, :rating, :edited, :user_id, :restaurant_id)
   end
-
+  
   def set_review
     @review = Review.find(params[:id])
   end
