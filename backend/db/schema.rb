1c4371c736d5e2b93e9f11d061c8e41ca0abe3c8
tiffnuugen <tiffanynuugen@gmail.com>
2020-07-11 21:53:22 -0400

    remove yelp_id column from saved restaurants table and update schema

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 2020_07_07_210557) do
 
   create_table "saved_restaurants", force: :cascade do |t|
     t.string "name"
-    t.string "yelp_id"
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
