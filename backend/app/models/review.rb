49bfd5daa033247d5379e2f7c4ce29b27ec6924b
tiffnuugen <tiffanynuugen@gmail.com>
2020-07-14 19:25:10 -0400

    remove :optional option from user and restaurant model

--- backend/app/models/review.rb
+++ backend/app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
-  belongs_to :user, optional: true
-  belongs_to :restaurant, optional: true, primary_key: 'yelp_id'
+  belongs_to :user
+  belongs_to :restaurant, primary_key: 'yelp_id'
   after_destroy :destroy_orphaned_restaurant
 
   def destroy_orphaned_restaurant
