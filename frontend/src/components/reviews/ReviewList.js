13c42fb306f32e9d575bad3c6bb1fc5202b0f99d
tiffnuugen <tiffanynuugen@gmail.com>
2020-07-20 18:48:45 -0400

    convert review list component into functional component

--- frontend/src/components/reviews/ReviewList.js
+++ frontend/src/components/reviews/ReviewList.js
@@ -1,59 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Divider, Header, Comment, Message } from 'semantic-ui-react';
 
 import Review from './Review';
 
-class ReviewList extends Component {
-  render() {
-    const reviews = this.props.reviews
-      .filter((review) => review.restaurant_id === this.props.restaurantId)
-      .sort((a, b) =>
-        Date.parse(a.created_at) < Date.parse(b.created_at) ? 1 : -1
-      );
-    const { currentUser, deleteReview, editReview } = this.props;
-    return (
-      <>
-        <Divider section horizontal>
-          <Header as='h2'>REVIEWS ({reviews.length})</Header>
-        </Divider>
-        {reviews.length > 0 ? (
-          <Comment.Group>
-            {reviews.map(
-              ({
-                id,
-                text,
-                rating,
-                edited,
-                created_at,
-                user: { username }
-              }) => (
-                <Review
-                  key={id}
-                  id={id}
-                  originalText={text}
-                  originalRating={rating}
-                  edited={edited}
-                  createdAt={created_at}
-                  username={username}
-                  currentUser={currentUser}
-                  deleteReview={deleteReview}
-                  editReview={editReview}
-                />
-              )
-            )}
-          </Comment.Group>
-        ) : (
-          <Message>
-            <Message.Header>No Reviews</Message.Header>
-            <p>
-              It looks like there are no reviews here yet. Be the first to leave
-              a review.
-            </p>
-          </Message>
-        )}
-      </>
+const ReviewList = ({
+  reviews,
+  restaurantId,
+  currentUser,
+  deleteReview,
+  editReview
+}) => {
+  const reviewsArr = reviews
+    .filter((review) => review.restaurant_id === restaurantId)
+    .sort((a, b) =>
+      Date.parse(a.created_at) < Date.parse(b.created_at) ? 1 : -1
     );
-  }
-}
+  return (
+    <>
+      <Divider section horizontal>
+        <Header as='h2'>REVIEWS ({reviewsArr.length})</Header>
+      </Divider>
+      {reviewsArr.length > 0 ? (
+        <Comment.Group>
+          {reviewsArr.map(
+            ({ id, text, rating, edited, created_at, user: { username } }) => (
+              <Review
+                key={id}
+                id={id}
+                originalText={text}
+                originalRating={rating}
+                edited={edited}
+                createdAt={created_at}
+                username={username}
+                currentUser={currentUser}
+                deleteReview={deleteReview}
+                editReview={editReview}
+              />
+            )
+          )}
+        </Comment.Group>
+      ) : (
+        <Message>
+          <Message.Header>No Reviews</Message.Header>
+          <p>
+            It looks like there are no reviews here yet. Be the first to leave a
+            review.
+          </p>
+        </Message>
+      )}
+    </>
+  );
+};
 
 export default ReviewList;
