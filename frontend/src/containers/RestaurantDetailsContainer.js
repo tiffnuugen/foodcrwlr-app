d27ea54b3dcdeb1735439e2844cd5a53ac9cf736
tiffnuugen <tiffanynuugen@gmail.com>
2020-07-21 02:00:19 -0400

    move all functions from restaurant details component to container

--- frontend/src/containers/RestaurantDetailsContainer.js
+++ frontend/src/containers/RestaurantDetailsContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
+
 import { connect } from 'react-redux';
-import { Divider } from 'semantic-ui-react';
+import { Divider, List } from 'semantic-ui-react';
 
 import RestaurantDetails from '../components/restaurants/RestaurantDetails';
-import ReviewsContainer from './ReviewsContainer';
+import ReviewListContainer from './ReviewListContainer';
 
 import {
   saveRestaurant,
@@ -23,27 +25,63 @@ class RestaurantDetailsContainer extends Component {
     this.fetchSavedRestaurants();
   }
 
-  render() {
-    const savedRestaurants = this.props.savedRestaurants.filter(
-      (rest) =>
-        rest.yelp_id === this.props.restaurantDetails.id &&
-        rest.user_id === this.props.currentUserId
+  handleSave = () => {
+    const {
+      restaurantDetails: { id, name },
+      saveRestaurant,
+      currentUserId
+    } = this.props;
+    axios
+      .post('http://localhost:3001/saved_restaurants', {
+        saved_restaurant: {
+          name: name,
+          yelp_id: id,
+          user_id: currentUserId
+        }
+      })
+      .then((res) => saveRestaurant(res.data));
+  };
+
+  handleUnsave = (id) => {
+    axios
+      .delete(`http://localhost:3001/saved_restaurants/${id}`)
+      .then((res) => this.props.unsaveRestaurant(res.data.id));
+  };
+
+  renderHours = (hours) => {
+    const parsedHours = hours[0].open;
+    const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    let currentDay;
+
+    return parsedHours.map(({ start, end, day }) => {
+      const startHour = start.slice(0, 2);
+      const startMin = start.slice(2);
+      const endHour = end.slice(0, 2);
+      const endMin = end.slice(2);
+      let dayOfOperation = '';
+      const hoursOfOperation = `${
+        ((parseInt(startHour) + 11) % 12) + 1
+      }:${startMin} ${startHour >= 12 ? 'pm' : 'am'} - ${
+        ((parseInt(endHour) + 11) % 12) + 1
+      }:${endMin} ${endHour >= 12 ? 'pm' : 'am'}`;
+
+      if (currentDay !== day) {
+        currentDay = day;
+        dayOfOperation = days[day];
+      }
+
+      return (
+        <List.Item key={uuidv4()}>
+          <strong>{dayOfOperation}</strong> {hoursOfOperation}
+        </List.Item>
     );
+    });
+  };
+
+  render() {
     return (
       <div className='ui text restaurant details container'>
-        {savedRestaurants.length === 0 ? (
-          <RestaurantDetails
-            restaurantDetails={this.props.restaurantDetails}
-            saveRestaurant={this.props.saveRestaurant}
-            unsaveRestaurant={this.props.unsaveRestaurant}
-            currentUserId={this.props.currentUserId}
-            loading={this.props.loading}
-          />
-        ) : (
-          savedRestaurants.map((savedRest) => (
             <RestaurantDetails
-              key={savedRest.id}
-              savedRest={savedRest}
               restaurantDetails={this.props.restaurantDetails}
               saveRestaurant={this.props.saveRestaurant}
               unsaveRestaurant={this.props.unsaveRestaurant}
