94676806f6da1d44891c5abc0e9a25d4c5ac35ba
tiffnuugen <tiffanynuugen@gmail.com>
2020-07-21 18:22:03 -0400

    convert review form component to functional component

--- frontend/src/components/reviews/ReviewForm.js
+++ frontend/src/components/reviews/ReviewForm.js
@@ -1,95 +1,35 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { connect } from 'react-redux';
+import React from 'react';
 import { Form, TextArea, Button, Icon, Rating } from 'semantic-ui-react';
 
-import { addReview } from '../../actions/apiActions';
+const ReviewForm = ({
+  text,
+  rating,
+  handleSubmit,
+  handleChange,
+  handleRate
+}) => {
+  return (
+    <Form className='review' onSubmit={handleSubmit}>
+      <strong>Select a rating:</strong>{' '}
+      <Rating
+        clearable
+        size='huge'
+        rating={rating}
+        maxRating={5}
+        onRate={handleRate}
+      />
+      <TextArea
+        name='text'
+        placeholder='Write a review...'
+        value={text}
+        onChange={handleChange}
+      />
+      <Button type='submit' floated='right' basic color='teal'>
+        <Icon name='add' />
+        Add Review
+      </Button>
+    </Form>
+  );
+};
 
-class ReviewForm extends Component {
-  state = {
-    text: '',
-    rating: 0
-  };
-
-  handleChange = (e) => {
-    this.setState({
-      text: e.target.value
-    });
-  };
-
-  handleRate = (e, { rating }) => {
-    this.setState({
-      rating
-    });
-  };
-
-  handleSubmit = async () => {
-    const { text, rating } = this.state;
-    const {
-      currentUserId,
-      restaurantId,
-      restaurantName,
-      yelpId,
-      addReview
-    } = this.props;
-
-    await axios.post('http://localhost:3001/restaurants', {
-      restaurant: {
-        name: restaurantName,
-        yelp_id: yelpId
-      }
-    });
-    const review = await axios.post('http://localhost:3001/reviews', {
-      review: {
-        text: text,
-        rating: rating,
-        user_id: currentUserId,
-        restaurant_id: restaurantId
-      }
-    });
-    addReview(review.data);
-    this.setState({
-      text: '',
-      rating: 0
-    });
-  };
-
-  render() {
-    const { text, rating } = this.state;
-    return (
-      <Form className='review' onSubmit={this.handleSubmit}>
-        <strong>Select a rating:</strong>{' '}
-        <Rating
-          clearable
-          size='huge'
-          rating={rating}
-          maxRating={5}
-          onRate={this.handleRate}
-        />
-        <TextArea
-          name='text'
-          placeholder='Write a review...'
-          value={text}
-          onChange={this.handleChange}
-        />
-        <Button type='submit' floated='right' basic color='teal'>
-          <Icon name='add' />
-          Add Review
-        </Button>
-      </Form>
-    );
-  }
-}
-
-const mapStateToProps = (state) => ({
-  restaurantId: state.yelp.restaurantDetails.id,
-  currentUserId: state.auth.user.id,
-  restaurantName: state.yelp.restaurantDetails.name,
-  yelpId: state.yelp.restaurantDetails.id
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  addReview: (review) => dispatch(addReview(review))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ReviewForm);
+export default ReviewForm;
