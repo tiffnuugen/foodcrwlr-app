d1eae6999d93a386ca8fb8a966d6696524178b10
tiffnuugen <tiffanynuugen@gmail.com>
2020-07-21 15:36:36 -0400

    convert restaurant form component to functional component

--- frontend/src/components/restaurants/RestaurantForm.js
+++ frontend/src/components/restaurants/RestaurantForm.js
@@ -1,77 +1,40 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import { Search, Button } from 'semantic-ui-react';
-import { withRouter } from 'react-router-dom';
 
-import { fetchRestaurants } from '../../actions/yelpActions';
+const RestaurantForm = ({
+  handleSearchChange,
+  handleSubmit,
+  term,
+  location,
+  loading
+}) => {
+  return (
+    <form className='search form container' onSubmit={handleSubmit}>
+      <Search
+        fluid
+        icon={null}
+        name='term'
+        placeholder='Search restaurants...'
+        showNoResults={false}
+        value={term}
+        onSearchChange={handleSearchChange}
+      />
+      <Search
+        fluid
+        icon={null}
+        name='location'
+        placeholder='near this location...'
+        showNoResults={false}
+        value={location}
+        onSearchChange={handleSearchChange}
+      />
+      {loading ? (
+        <Button circular size='large' loading icon='user' />
+      ) : (
+        <Button circular size='large' icon='search' />
+      )}
+    </form>
+  );
+};
 
-class RestaurantForm extends Component {
-  state = {
-    term: '',
-    location: ''
-  };
-
-  handleSearchChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const { term, location } = this.state;
-    const { fetchRestaurants, history } = this.props;
-    fetchRestaurants({
-      term: term,
-      location: location
-    });
-    history.push(`/search?term=${term}&location=${location}`);
-    this.setState({
-      term: '',
-      location: ''
-    });
-  };
-
-  render() {
-    const { term, location } = this.state;
-    return (
-      <form className='search form container' onSubmit={this.handleSubmit}>
-        <Search
-          fluid
-          icon={null}
-          name='term'
-          placeholder='Search restaurants...'
-          showNoResults={false}
-          value={term}
-          onSearchChange={this.handleSearchChange}
-        />
-        <Search
-          fluid
-          icon={null}
-          name='location'
-          placeholder='near this location...'
-          showNoResults={false}
-          value={location}
-          onSearchChange={this.handleSearchChange}
-        />
-        {this.props.loading ? (
-          <Button circular size='large' loading icon='user' />
-        ) : (
-          <Button circular size='large' icon='search' />
-        )}
-      </form>
-    );
-  }
-}
-
-const mapStateToProps = (state) => ({
-  loading: state.yelp.loading
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  fetchRestaurants: (searchValues) => dispatch(fetchRestaurants(searchValues))
-});
-
-export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(RestaurantForm)
-);
+export default RestaurantForm;
