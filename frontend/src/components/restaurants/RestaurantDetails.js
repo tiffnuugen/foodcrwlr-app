b452feeb3d4d3d2a011df5b31a2e94f839c6d780
tiffnuugen <tiffanynuugen@gmail.com>
2020-07-21 01:54:37 -0400

    remove bang operator from each condition and switch expressions around

--- frontend/src/components/restaurants/RestaurantDetails.js
+++ frontend/src/components/restaurants/RestaurantDetails.js
@@ -18,157 +18,146 @@ const RestaurantDetails = ({
   handleUnsave,
   renderHours,
   savedRestaurants,
-      loading,
+  loading,
   currentUserId,
-      restaurantDetails: {
-        id,
-        name,
-        image_url,
-        is_closed,
-        url,
-        display_phone,
-        categories,
-        rating,
-        location,
-        photos,
-        price,
-        hours
-      }
+  restaurantDetails: {
+    id,
+    name,
+    image_url,
+    is_closed,
+    url,
+    display_phone,
+    categories,
+    rating,
+    location,
+    photos,
+    price,
+    hours
+  }
 }) => {
   const savedRestaurant =
     savedRestaurants.find(
       (savedRest) =>
         savedRest.yelp_id === id && savedRest.user_id === currentUserId
     ) || !!undefined;
-    return (
-      <>
-        {loading ? (
-          <Dimmer active inverted>
-            <Loader size='large'>Loading...</Loader>
-          </Dimmer>
-        ) : (
-          <Segment>
-            <Item.Group>
-              <Item>
-                <Image
-                  src={image_url}
-                  alt='Not Available'
-                  height={300}
-                  width={300}
-                />
-                <Item.Content>
-                  <Segment>
-                    <Header
-                      size='huge'
-                      color='teal'
-                      as='a'
-                      href={url}
-                      target='_blank'
-                    >
-                      {name}
-                    </Header>
+  return (
+    <>
+      {loading ? (
+        <Dimmer active inverted>
+          <Loader size='large'>Loading...</Loader>
+        </Dimmer>
+      ) : (
+        <Segment>
+          <Item.Group>
+            <Item>
+              <Image
+                src={image_url}
+                alt='Not Available'
+                height={300}
+                width={300}
+              />
+              <Item.Content>
+                <Segment>
+                  <Header
+                    size='huge'
+                    color='teal'
+                    as='a'
+                    href={url}
+                    target='_blank'
+                  >
+                    {name}
+                  </Header>
                   {savedRestaurant ? (
-                      <div className='save restaurant'>
-                        <Button
-                          active
-                          circular
-                          basic
-                          icon='bookmark outline'
+                    <div className='save restaurant'>
+                      <Button
+                        active
+                        circular
+                        basic
+                        icon='bookmark outline'
                         onClick={() => handleUnsave(savedRestaurant.id)}
-                        />
-                        <Label size='mini'>Saved</Label>
-                      </div>
-                    ) : (
-                      <div className='save restaurant'>
-                        <Button
-                          circular
-                          basic
-                          icon='bookmark outline'
-                        onClick={handleSave}
-                        />
-                        <Label size='mini'>Save</Label>
-                      </div>
-                    )}
-                    <Item.Meta>
-                      {!categories ? 'Not Available' : categories[0].title}
-                    </Item.Meta>
-                    <Item.Meta>
-                      {price &&
-                        price
-                          .split('')
-                          .map(() => (
-                            <Icon key={uuidv4()} fitted name='dollar sign' />
-                          ))}{' '}
-                      {price && '|'}{' '}
-                      <strong>
-                        {rating % 1 === 0 ? `${rating}.0` : rating}
-                      </strong>
-                      <Rating
-                        disabled
-                        size='large'
-                        defaultRating={rating}
-                        maxRating={5}
                       />
-                    </Item.Meta>
-                    <Segment.Group>
-                      <Segment textAlign='center' className='location'>
-                        <Item.Description>
-                          <Icon name='map marker alternate' />
-                          <strong>
-                            {!location
-                              ? 'Not Available'
-                              : location.display_address.map(
-                                  (item, index) => `${index ? ', ' : ''}${item}`
-                                )}
-                          </strong>
-                        </Item.Description>
-                      </Segment>
-                      <Segment
-                        size='large'
-                        textAlign='center'
-                        className='hours'
-                      >
-                        <Icon name='clock outline' />
-                        <Item.Description>
-                          {!hours ? (
-                            <strong>Not Available</strong>
-                          ) : (
-                            this.renderHours(hours)
-                          )}
-                        </Item.Description>
-                      </Segment>
-                      <Segment
-                        size='large'
-                        textAlign='center'
-                        className='phone'
-                      >
-                        <Icon name='phone' />
+                      <Label size='mini'>Saved</Label>
+                    </div>
+                  ) : (
+                    <div className='save restaurant'>
+                      <Button
+                        circular
+                        basic
+                        icon='bookmark outline'
+                        onClick={handleSave}
+                      />
+                      <Label size='mini'>Save</Label>
+                    </div>
+                  )}
+                  <Item.Meta>
+                    {categories ? categories[0].title : 'Not Available'}
+                  </Item.Meta>
+                  <Item.Meta>
+                    {price &&
+                      price
+                        .split('')
+                        .map(() => (
+                          <Icon key={uuidv4()} fitted name='dollar sign' />
+                        ))}{' '}
+                    {price && '|'}{' '}
+                    <strong>{rating % 1 === 0 ? `${rating}.0` : rating}</strong>
+                    <Rating
+                      disabled
+                      size='large'
+                      defaultRating={rating}
+                      maxRating={5}
+                    />
+                  </Item.Meta>
+                  <Segment.Group>
+                    <Segment textAlign='center' className='location'>
+                      <Item.Description>
+                        <Icon name='map marker alternate' />
                         <strong>
-                          {!display_phone ? 'Not Available' : display_phone}
+                          {location
+                            ? location.display_address.map(
+                                (item, index) => `${index ? ', ' : ''}${item}`
+                              )
+                            : 'Not Available'}
                         </strong>
-                      </Segment>
-                    </Segment.Group>
-                    <Item.Extra>
-                      {!categories ? (
-                        <Label>Not Available</Label>
-                      ) : (
-                        categories.map(({ alias }) => (
-                          <Label key={alias} size='large'>
-                            {alias}
-                          </Label>
-                        ))
-                      )}
-                    </Item.Extra>
-                  </Segment>
-                </Item.Content>
-              </Item>
-            </Item.Group>
-          </Segment>
-        )}
-      </>
-    );
-  }
-}
+                      </Item.Description>
+                    </Segment>
+                    <Segment size='large' textAlign='center' className='hours'>
+                      <Icon name='clock outline' />
+                      <Item.Description>
+                        {hours ? (
+                          renderHours(hours)
+                        ) : (
+                          <strong>Not Available</strong>
+                        )}
+                      </Item.Description>
+                    </Segment>
+                    <Segment size='large' textAlign='center' className='phone'>
+                      <Icon name='phone' />
+                      <strong>
+                        {display_phone ? display_phone : 'Not Available'}
+                      </strong>
+                    </Segment>
+                  </Segment.Group>
+                  <Item.Extra>
+                    {categories ? (
+                      categories.map(({ alias }) => (
+                        <Label key={alias} size='large'>
+                          {alias}
+                        </Label>
+                      ))
+                    ) : (
+                      <Label>Not Available</Label>
+                    )}
+                  </Item.Extra>
+                </Segment>
+              </Item.Content>
+            </Item>
+          </Item.Group>
+        </Segment>
+      )}
+    </>
+  );
+};
 
 export default RestaurantDetails;
 
