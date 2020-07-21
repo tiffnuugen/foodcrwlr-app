48be63680d6db54232ab133a71911f36456b3dea
tiffnuugen <tiffanynuugen@gmail.com>
2020-07-21 18:23:17 -0400

    move all functions from review form component to review list container

--- frontend/src/containers/ReviewListContainer.js
+++ frontend/src/containers/ReviewListContainer.js
@@ -20,6 +20,49 @@ class ReviewListContainer extends Component {
     this.fetchReviews();
   }
 
+  handleChange = (e) => {
+    this.setState({
+      text: e.target.value
+    });
+  };
+
+  handleRate = (e, { rating }) => {
+    this.setState({
+      rating
+    });
+  };
+
+  handleSubmit = async () => {
+    const { text, rating } = this.state;
+    const {
+      currentUserId,
+      restaurantId,
+      restaurantName,
+      yelpId,
+      addReview
+    } = this.props;
+
+    await axios.post('http://localhost:3001/restaurants', {
+      restaurant: {
+        name: restaurantName,
+        yelp_id: yelpId
+      }
+    });
+    const review = await axios.post('http://localhost:3001/reviews', {
+      review: {
+        text: text,
+        rating: rating,
+        user_id: currentUserId,
+        restaurant_id: restaurantId
+      }
+    });
+    addReview(review.data);
+    this.setState({
+      text: '',
+      rating: 0
+    });
+  };
+
   render() {
     return (
       <div className='review list container'>
