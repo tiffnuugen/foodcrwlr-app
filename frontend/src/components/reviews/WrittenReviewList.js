a2d4a1070029ec11b942b38e9b83ad358e1cb18c
tiffnuugen <tiffanynuugen@gmail.com>
2020-07-22 22:58:03 -0400

    build written review list component

--- frontend/src/components/reviews/WrittenReviewList.js
+++ frontend/src/components/reviews/WrittenReviewList.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { Item, Rating } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
-const WrittenReviewList = () => {
-  return <div>WrittenReviewList</div>;
+const WrittenReviewList = ({
+  reviews,
+  showRestaurantDetails,
+  currentUserId
+}) => {
+  const writtenReviewsArr = reviews.filter(
+    (review) => review.user_id === currentUserId
+  );
+  return (
+    <Item.Group divided>
+      <h1>MY REVIEWS</h1>
+      {writtenReviewsArr.map(
+        ({ id, text, rating, restaurant_id, restaurant: { name } }) => (
+          <Item key={id}>
+            <Item.Content>
+              <Item.Header
+                as={Link}
+                to={`/restaurants/${restaurant_id}`}
+                onClick={() => showRestaurantDetails(restaurant_id)}
+              >
+                {name}
+              </Item.Header>
+              <Item.Extra>
+                <Rating disabled defaultRating={rating} maxRating={5} />
+              </Item.Extra>
+              <Item.Meta>"{text}"</Item.Meta>
+            </Item.Content>
+          </Item>
+        )
+      )}
+    </Item.Group>
+  );
 };
 
 export default WrittenReviewList;
