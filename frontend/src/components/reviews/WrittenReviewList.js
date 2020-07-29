bd0f0102d305e5703959f4fbef49b19648d51806
tiffnuugen <tiffanynuugen@gmail.com>
2020-07-29 19:24:08 -0400

    create conditional logic to display message when there are no written reviews

--- frontend/src/components/reviews/WrittenReviewList.js
+++ frontend/src/components/reviews/WrittenReviewList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Item, Rating } from 'semantic-ui-react';
+import { Item, Rating, Message } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 const WrittenReviewList = ({ reviews, currentUserId }) => {
@@ -9,20 +9,27 @@ const WrittenReviewList = ({ reviews, currentUserId }) => {
   return (
     <Item.Group divided>
       <h1>MY REVIEWS</h1>
-      {writtenReviewsArr.map(
-        ({ id, text, rating, restaurant_id, restaurant: { name } }) => (
-          <Item key={id}>
-            <Item.Content>
+      {writtenReviewsArr.length > 0 ? (
+        writtenReviewsArr.map(
+          ({ id, text, rating, restaurant_id, restaurant: { name } }) => (
+            <Item key={id}>
+              <Item.Content>
                 <Item.Header as={Link} to={`/restaurants/${restaurant_id}`}>
-                {name}
-              </Item.Header>
-              <Item.Extra>
-                <Rating disabled defaultRating={rating} maxRating={5} />
-              </Item.Extra>
-              <Item.Meta>"{text}"</Item.Meta>
-            </Item.Content>
-          </Item>
+                  {name}
+                </Item.Header>
+                <Item.Extra>
+                  <Rating disabled defaultRating={rating} maxRating={5} />
+                </Item.Extra>
+                <Item.Meta>"{text}"</Item.Meta>
+              </Item.Content>
+            </Item>
+          )
         )
+      ) : (
+        <Message>
+          <Message.Header>No Written Reviews</Message.Header>
+          <p>You have not written any reviews yet. Write a review now!</p>
+        </Message>
       )}
     </Item.Group>
   );
