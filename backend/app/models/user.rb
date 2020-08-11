1e668a41e201d09014567300f644858694817064
tiffnuugen <tiffanynuugen@gmail.com>
2020-08-11 14:48:06 -0400

    add backend validation for username to be a minimum of three characters

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
   has_many :reviews
   has_many :restaurants, through: :reviews
   has_many :saved_restaurants
