b8723a096a8222be813f1e7c90ac00a4428563dc
tiffnuugen <tiffanynuugen@gmail.com>
2020-08-11 12:44:59 -0400

    create hot and new action

--- backend/app/controllers/yelp_controller.rb
+++ backend/app/controllers/yelp_controller.rb
@@ -10,6 +10,17 @@ class YelpController < ApplicationController
     render json: search_results
   end
 
+  def hot_and_new
+    res = Faraday.get("https://api.yelp.com/v3/businesses/search") do |req|
+      req.headers['Authorization'] = "Bearer #{ENV['API_KEY']}"
+      req.params['categories'] = 'food,restaurants'
+      req.params['location'] = 'nyc'
+      req.params['attributes'] = 'hot_and_new'
+    end
+    search_results = JSON.parse(res.body)
+    render json: search_results
+  end
+
   def display
     res = Faraday.get("https://api.yelp.com/v3/businesses/#{params[:id]}") do |req|
       req.headers['Authorization'] = "Bearer #{ENV['API_KEY']}"
